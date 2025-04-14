ee462e56b54e0164579bd7c01c03f51d52323912
long12345mk <125982470+long12345mk@users.noreply.github.com>
2025-04-14 16:53:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,26 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     }
   });
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
   
